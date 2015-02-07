a680aced8447fa18b1ce28117780a436924cbd28
nemo <nemo1211@gmail.com>
2015-02-07 15:54:40 -0500

    Version bump reflecting data shrinking option

--- R/twitter.R
+++ R/twitter.R
@@ -1,7 +1,7 @@
 ####################################################################
 ##
 ## MassMine: twitter module
-## Copyright (C) 2014  Nicholas M. Van Horn & Aaron Beveridge
+## Copyright (C) 2014-2015  Nicholas M. Van Horn & Aaron Beveridge
 ## 
 ##  This program is free software: you can redistribute it and/or modify
 ##  it under the terms of the GNU General Public License as published by
