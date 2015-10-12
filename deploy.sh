454e9a84754f88f6c5133f3865bce7deab08bf1d
Nicholas M. VanHorn <nvanhorn@app-c07pl0akg1j0.capital.edu>
2015-10-12 15:20:15 -0400

    OS X build script

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,38 @@
 # Produces compressed directories containing binary distributions for massmine
 if [ "$(uname)" == "Darwin" ]
 then
- echo This script does not support Mac OS X platform yet
+    # The executable and linked library
+    csc -deploy massmine.scm
+
+    # This step is currently required because of a bug in chicken/openssl
+    # that prevents openssl from being loaded in `eval`. This command
+    # copies CHICKEN's core libraries into the deployment directory. The
+    # bug is reported at https://bugs.call-cc.org/ticket/1191
+    
+    # To solve the above problem, I have built a local version of the
+    # http-client egg that simply adds openssl to the use statement at
+    # the beginning of the egg. Installing it manually from its
+    # directory (using `sudo chicken-install` from its directory)
+    # makes everything work as expected on OS X
+    cd ../chicken/http-client/
+    sudo chicken-install -deploy -p ../../massmine/massmine
+    cd ../../massmine/
+    
+    # Install clucker and additional eggs
+    sudo chicken-install -deploy -p ./massmine clucker args medea openssl srfi-19
+
+    # Package everything up with the current version number (zip and tarball)
+    zip massmine-`./massmine/massmine -v | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+'`-OSX-10.10.zip -r ./massmine/
+
+    tar czf massmine-`./massmine/massmine -v | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+'`-OSX-10.10.tar.gz ./massmine/
+
+    # Clean up by moving the results out of the development directory
+    mv massmine-`./massmine/massmine -v | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+'`-OSX-10.10.zip ~/deploy
+    mv massmine-`./massmine/massmine -v | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+'`-OSX-10.10.tar.gz ~/deploy
+
+    # Remove the build directory
+    rm -rf massmine/
+
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]
 then
     # Do something under Linux platform
